c231ff24d32f35d1bd8eb32f5112c224d7263d4d
Angie Garcia <dev05@aiatic.com>
2022-07-03 18:41:21 -0500

    feat: pruebas iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+  public nombre:string = 'angie'
+   public mostrarNombre() {
+    console.log(this.nombre)
+   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+ //Modulo personalizado
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule
+/*     ButtonModule,
+    CardModule */
   ],
   providers: [],
   bootstrap: [AppComponent]
